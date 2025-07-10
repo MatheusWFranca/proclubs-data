e239dcd842957c5d4ef45967bdd90040ab99c173
MatheusWFranca <mateusanches2@gmail.com>
2025-07-10 20:58:46 -0300

    pequenos ajustes

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ now = datetime.now().strftime('%Y%m%d')
 
 base_url = 'https://proclubs.ea.com/api/fc'
 
-
-
 headers = {
     'sec-ch-ua-platform': '"macOS"',
     'Referer': 'https://www.ea.com/',
@@ -20,12 +18,7 @@ headers = {
     'sec-ch-ua-mobile': '?0',
 }
 
-
-
-
-
 def get_club_members():
-
     params = {
     'platform': 'common-gen5',
     'clubId': '4606176',
@@ -35,18 +28,14 @@ def get_club_members():
         response = requests.get(f'{base_url}/members/career/stats', params=params, headers=headers)
         if response.status_code == 200:
             data = response.json()
-
             df = pd.DataFrame(data['members'])
-
             df.to_csv(f'history/{now}_stats.csv', index=False)
         else:
             print(response.status_code)
     except KeyError:
         print(KeyError)
 
-
 def get_club_stats():
-    
     params = {
     'platform': 'common-gen5',
     'clubIds': '4606176',
@@ -57,16 +46,13 @@ def get_club_stats():
         if response.status_code == 200:
             data = response.json()
             df = pd.DataFrame.from_dict(data[str(club_id)]['customKit'], orient='index', columns=['value'])
-            
             df.to_csv(f'history/{now}_club_stats.csv', index=True)
-
         else:
             print(response.status_code)
     except NameError:
         print(NameError)
 
 def get_club_overall_stats():
-
     params = {
     'platform': 'common-gen5',
     'clubIds': '4606176',
@@ -74,23 +60,17 @@ def get_club_overall_stats():
 
     try:
         response = requests.get(f'{base_url}/clubs/overallStats', params=params, headers=headers)
-
         if response.status_code == 200:
             data = response.json()
-
             df = pd.DataFrame(data)
-
             df = df.T
-            
             df.to_csv(f'history/{now}_club_overral_stats.csv', index=True)
         else: 
             print(response.status_code)
-
     except NameError:
         print(NameError)
 
 def get_members_detailed_stats():
-
     params = {
         'platform': 'common-gen5',
         'clubId': '4606176',
@@ -98,23 +78,16 @@ def get_members_detailed_stats():
 
     try:
         response = requests.get(f'{base_url}/members/stats', params=params, headers=headers)
-
         if response.status_code == 200:
             data = response.json()
-
             df = pd.DataFrame(data['members'])
-
             df.to_json(f'history/{now}_members_detailed_stats.json', orient='records', indent=2, index=False)
-            
-
         else: 
             print(response.status_code)
-
     except NameError:
         print(NameError)
 
 def get_lastest_games():
-
     params = {
         'platform': 'common-gen5',
         'clubIds': '4606176',
@@ -124,22 +97,14 @@ def get_lastest_games():
 
     try:
         response = requests.get(f'{base_url}/clubs/matches', params=params, headers=headers)
-
         if response.status_code == 200:
             data = response.json()
-
             df = pd.DataFrame(data)
-
             df.to_json(f'history/{now}_get_latest_games.json', orient='records', indent=2, index=False)
-
         else: 
             print(response.status_code)
-
     except NameError:
         print(NameError)
-
-
-
 
 def main():
     get_club_members()
@@ -147,7 +112,6 @@ def main():
     get_club_overall_stats()
     get_members_detailed_stats()
     get_lastest_games()
-    
 
 if __name__ == "__main__":
     main()
